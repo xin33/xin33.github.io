11775e22a7e3d130cd4fd30f27eb7a9b24b8a2bb
xin33 <dx1281181508@163.com>
2019-07-18 20:43:59 +0800

    lodash1

--- miao/lodash/xin33-lodash.js
+++ miao/lodash/xin33-lodash.js
@@ -1,5 +1,17 @@
+
 var xin33 = {
-    compact: function(ary) {
+  compact: function(ary) {
     return ary.filter(it => it)
   },
+  chunk:function(ary , size){
+    var result = []
+    while(ary.length){
+      result.concat(ary.splice(0 ,size))
+    }
+    return result
+  },
+  difference:function(ary , values){
+    return ary.filter(it => values.indexOf(it) == -1)
+  },
+  
 }
